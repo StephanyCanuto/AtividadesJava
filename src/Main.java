ba3aa2db8aab49d94910f96521a1814e9806ff16
StephanyCanuto <stephany.canutos25@gmail.com>
2025-03-15 10:42:32 -0300

    feat: atividade Java resolção de problemas

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+       //media de 3 números
+
+        int n1 = 10;
+        int n2 = 8;
+        int n3 = 12;
+
+        double mediaReal = (n1+n2+n3)/3.0;
+        int mediaInt = (n1+n2+n3)/3;
+        float mediaFloat = (float) ((n1+n2+n3)/3.0f);
+        long mediaLong = (long) ((n1+n2+n3)/3L);
+        System.out.println("Os números são: "+ n1 + ", "+ n2+ ", "+ n3);
+        System.out.println("A média do número Real é: "+ mediaReal);
+        System.out.println("A média do número inteiro é: "+ mediaInt);
+        System.out.println("A média do número tipo float é: "+ mediaFloat);
+        System.out.println("A média do número tipo long é: "+ mediaLong);
+
+    }
+}
